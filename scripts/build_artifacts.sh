2361444951e17847bfc662af56ad7db9376d22d1
Spike Spiegel <spikeonmars@protonmail.com>
2022-02-04 08:52:48 -0300

    Update cosmwasm optimizer version

--- scripts/build_artifacts.sh
+++ scripts/build_artifacts.sh
@@ -6,4 +6,4 @@ set -o pipefail
 docker run --rm -v "$(pwd)":/code \
     --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
     --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
-    cosmwasm/workspace-optimizer:0.11.5
+    cosmwasm/workspace-optimizer:0.12.5
