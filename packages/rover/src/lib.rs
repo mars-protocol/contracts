4ad7bf0a798672572bcb295d424b7d36ba71e7f9
larry <26318510+larry0x@users.noreply.github.com>
2022-06-29 11:55:49 +0100

    Reorganize rover package; use unchecked types in msgs/responses

--- packages/rover/src/lib.rs
+++ packages/rover/src/lib.rs
@@ -1,2 +1,33 @@
-pub mod messages;
-pub mod types;
+use cosmwasm_std::Addr;
+use cw_asset::AssetInfoUnchecked;
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
+pub struct InstantiateMsg {
+    pub owner: String,
+    pub allowed_vaults: Vec<String>,
+    pub allowed_assets: Vec<AssetInfoUnchecked>,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum ExecuteMsg {}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum QueryMsg {
+    GetOwner {},
+    GetAllowLists {},
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct OwnerResponse {
+    pub owner: Addr,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct AllowListsResponse {
+    pub vaults: Vec<Addr>,
+    pub assets: Vec<AssetInfoUnchecked>,
+}
