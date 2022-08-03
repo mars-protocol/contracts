91b9cfb0964629868487c8a7dc4e990054d2288e
larry <26318510+larry0x@users.noreply.github.com>
2022-08-03 04:55:47 +0100

    adjust exports

--- contracts/osmosis/mars-oracle/src/lib.rs
+++ contracts/osmosis/mars-oracle/src/lib.rs
@@ -1,11 +1,10 @@
 pub mod contract;
 mod error;
 mod helpers;
-mod msg;
+pub mod msg;
 pub mod state;
 
 pub use error::{ContractError, ContractResult};
-pub use msg::{ExecuteMsg, PriceSource, PriceSourceResponse};
 
 #[cfg(test)]
 mod testing;
--- contracts/osmosis/mars-oracle/src/state.rs
+++ contracts/osmosis/mars-oracle/src/state.rs
@@ -3,7 +3,7 @@ use cw_storage_plus::{Item, Map};
 
 use mars_outpost::oracle::Config;
 
-use crate::PriceSource;
+use crate::msg::PriceSource;
 
 pub const CONFIG: Item<Config<Addr>> = Item::new("config");
 pub const PRICE_SOURCES: Map<String, PriceSource> = Map::new("price_sources");
