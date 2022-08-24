89b0a020b2414e84a8457eec883e108534d398c5
brimigs <85972460+brimigs@users.noreply.github.com>
2022-08-24 10:51:16 +0100

    Removing Terra Classic
    Removing files related to Mars deployment on Terra Classic and Mars CW20 tokens along with related dependencies

--- scripts/deploy/deploy_helpers.ts
+++ scripts/deploy/deploy_helpers.ts
@@ -3,12 +3,12 @@ import path from 'path';
 
 export const ARTIFACTS_PATH = '../artifacts';
 
-export function writeArtifact(data: object, name: string = 'artifact') {
+export function writeArtifact(data: object, name = 'artifact') {
   writeFileSync(path.join(ARTIFACTS_PATH, `${name}.json`), JSON.stringify(data, null, 2));
 }
 
 // Reads json containing contract addresses located in /artifacts folder for specified network.
-export function readArtifact(name: string = 'artifact') {
+export function readArtifact(name = 'artifact') {
   try {
     const data = readFileSync(path.join(ARTIFACTS_PATH, `${name}.json`), 'utf8');
     return JSON.parse(data);
