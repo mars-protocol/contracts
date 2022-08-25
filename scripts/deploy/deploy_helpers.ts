329163bd2d86637d3ba02a63e4350cbf25fcdbff
brimigs <85972460+brimigs@users.noreply.github.com>
2022-08-25 16:19:55 +0100

    Updated Prettierrc to align with FE team
    Updated prettierrc and ran command

--- scripts/deploy/deploy_helpers.ts
+++ scripts/deploy/deploy_helpers.ts
@@ -1,18 +1,24 @@
-import { readFileSync, writeFileSync } from 'fs';
-import path from 'path';
+import { readFileSync, writeFileSync } from 'fs'
+import path from 'path'
 
-export const ARTIFACTS_PATH = '../artifacts';
+export const ARTIFACTS_PATH = '../artifacts'
 
 export function writeArtifact(data: object, name = 'artifact') {
-  writeFileSync(path.join(ARTIFACTS_PATH, `${name}.json`), JSON.stringify(data, null, 2));
+    writeFileSync(
+        path.join(ARTIFACTS_PATH, `${name}.json`),
+        JSON.stringify(data, null, 2)
+    )
 }
 
 // Reads json containing contract addresses located in /artifacts folder for specified network.
 export function readArtifact(name = 'artifact') {
-  try {
-    const data = readFileSync(path.join(ARTIFACTS_PATH, `${name}.json`), 'utf8');
-    return JSON.parse(data);
-  } catch (e) {
-    return {};
-  }
+    try {
+        const data = readFileSync(
+            path.join(ARTIFACTS_PATH, `${name}.json`),
+            'utf8'
+        )
+        return JSON.parse(data)
+    } catch (e) {
+        return {}
+    }
 }
