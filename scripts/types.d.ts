a19066070f76ceb941afe8a87cdbcaf4533476ef
brimigs <85972460+brimigs@users.noreply.github.com>
2022-09-01 07:00:16 -0400

    Mp 1147 add linter and remove terra related files  (#47)
    * Added linter and prettierrc [MP-1147]
    
    * Removing Terra Classic
    
    Removing files related to Mars deployment on Terra Classic and Mars CW20 tokens along with related dependencies
    
    * fix linter/ prettierrc errors
    
    * Updated all packages to latest version
    
    * Updated Prettierrc to align with FE team
    
    Updated prettierrc and ran command
    
    * Removed Javascript files

--- scripts/types.d.ts
+++ scripts/types.d.ts
@@ -1,123 +1,123 @@
 interface CouncilInitMsg {
-  config: {
-    address_provider_address?: string
-    proposal_voting_period: number
-    proposal_effective_delay: number
-    proposal_expiration_period: number
-    proposal_required_deposit: string
-    proposal_required_quorum: string
-    proposal_required_threshold: string
-  }
+    config: {
+        address_provider_address?: string
+        proposal_voting_period: number
+        proposal_effective_delay: number
+        proposal_expiration_period: number
+        proposal_required_deposit: string
+        proposal_required_quorum: string
+        proposal_required_threshold: string
+    }
 }
 
 interface VestingInitMsg {
-  address_provider_address?: string
-  unlock_schedule: Schedule
+    address_provider_address?: string
+    unlock_schedule: Schedule
 }
 
 interface Schedule {
-  start_time: number
-  cliff: number
-  duration: number
+    start_time: number
+    cliff: number
+    duration: number
 }
 
 interface StakingInitMsg {
-  config: {
-    owner?: string
-    address_provider_address?: string
-    astroport_factory_address?: string
-    astroport_max_spread: string
-    cooldown_duration: number
-    unstake_window: number
-  }
+    config: {
+        owner?: string
+        address_provider_address?: string
+        astroport_factory_address?: string
+        astroport_max_spread: string
+        cooldown_duration: number
+        unstake_window: number
+    }
 }
 
 interface SafetyFundInitMsg {
-  owner?: string
-  astroport_factory_address?: string
-  astroport_max_spread: string
+    owner?: string
+    astroport_factory_address?: string
+    astroport_max_spread: string
 }
 
 interface TreasuryInitMsg {
-  owner?: string
-  astroport_factory_address?: string
-  astroport_max_spread: string
+    owner?: string
+    astroport_factory_address?: string
+    astroport_max_spread: string
 }
 
 interface ProtocolRewardsCollectorInitMsg {
-  config: {
-    owner?: string,
-    address_provider_address?: string,
-    safety_fund_fee_share: string,
-    treasury_fee_share: string,
-    astroport_factory_address?: string,
-    astroport_max_spread: string,
-  }
+    config: {
+        owner?: string
+        address_provider_address?: string
+        safety_fund_fee_share: string
+        treasury_fee_share: string
+        astroport_factory_address?: string
+        astroport_max_spread: string
+    }
 }
 
 interface RedBankInitMsg {
-  config: {
-    owner?: string
-    address_provider_address?: string
-    ma_token_code_id?: number
-    close_factor: string
-  }
+    config: {
+        owner?: string
+        address_provider_address?: string
+        ma_token_code_id?: number
+        close_factor: string
+    }
 }
 
 interface DynamicInterestRate {
-  dynamic: {
-    min_borrow_rate: string
-    max_borrow_rate: string
-    kp_1: string
-    optimal_utilization_rate: string
-    kp_augmentation_threshold: string
-    kp_2: string
-    update_threshold_txs: number
-    update_threshold_seconds: number
-  }
+    dynamic: {
+        min_borrow_rate: string
+        max_borrow_rate: string
+        kp_1: string
+        optimal_utilization_rate: string
+        kp_augmentation_threshold: string
+        kp_2: string
+        update_threshold_txs: number
+        update_threshold_seconds: number
+    }
 }
 
 interface LinearInterestRate {
-  linear: {
-    optimal_utilization_rate: string
-    base: string
-    slope_1: string
-    slope_2: string
-  }
+    linear: {
+        optimal_utilization_rate: string
+        base: string
+        slope_1: string
+        slope_2: string
+    }
 }
 
 interface InitOrUpdateAssetParams {
-  initial_borrow_rate: string
-  max_loan_to_value: string
-  reserve_factor: string
-  liquidation_threshold: string
-  liquidation_bonus: string
-  interest_rate_model_params: DynamicInterestRate | LinearInterestRate
-  active: boolean
-  deposit_enabled: boolean
-  borrow_enabled: boolean
+    initial_borrow_rate: string
+    max_loan_to_value: string
+    reserve_factor: string
+    liquidation_threshold: string
+    liquidation_bonus: string
+    interest_rate_model_params: DynamicInterestRate | LinearInterestRate
+    active: boolean
+    deposit_enabled: boolean
+    borrow_enabled: boolean
 }
 
 interface Asset {
-  denom?: string
-  symbol?: string
-  contract_addr?: string
-  init_params: InitOrUpdateAssetParams
+    denom?: string
+    symbol?: string
+    contract_addr?: string
+    init_params: InitOrUpdateAssetParams
 }
 
 interface Config {
-  councilInitMsg: CouncilInitMsg
-  vestingInitMsg: VestingInitMsg
-  stakingInitMsg: StakingInitMsg
-  safetyFundInitMsg: SafetyFundInitMsg
-  treasuryInitMsg: TreasuryInitMsg
-  protocolRewardsCollectorInitMsg: ProtocolRewardsCollectorInitMsg
-  redBankInitMsg: RedBankInitMsg
-  initialAssets: Asset[]
-  mirFarmingStratContractAddress: string | undefined
-  ancFarmingStratContractAddress: string | undefined
-  marsFarmingStratContractAddress: string | undefined
-  minterProxyContractAddress: string | undefined
-  marsTokenContractAddress: string | undefined
-  oracleFactoryAddress: string | undefined
+    councilInitMsg: CouncilInitMsg
+    vestingInitMsg: VestingInitMsg
+    stakingInitMsg: StakingInitMsg
+    safetyFundInitMsg: SafetyFundInitMsg
+    treasuryInitMsg: TreasuryInitMsg
+    protocolRewardsCollectorInitMsg: ProtocolRewardsCollectorInitMsg
+    redBankInitMsg: RedBankInitMsg
+    initialAssets: Asset[]
+    mirFarmingStratContractAddress: string | undefined
+    ancFarmingStratContractAddress: string | undefined
+    marsFarmingStratContractAddress: string | undefined
+    minterProxyContractAddress: string | undefined
+    marsTokenContractAddress: string | undefined
+    oracleFactoryAddress: string | undefined
 }
