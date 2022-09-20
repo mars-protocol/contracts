ae9d396ca9818ffc613a480e9b1025cadbc52acf
Piotr Babel <piotr.babel@gmail.com>
2022-09-20 15:24:10 +0200

    Fix todo for config.

--- contracts/red-bank/src/state.rs
+++ contracts/red-bank/src/state.rs
@@ -3,7 +3,7 @@ use cw_storage_plus::{Item, Map};
 
 use mars_outpost::red_bank::{Collateral, Config, Debt, Market};
 
-pub const CONFIG: Item<Config> = Item::new("config");
+pub const CONFIG: Item<Config<Addr>> = Item::new("config");
 pub const MARKETS: Map<&str, Market> = Map::new("markets");
 pub const COLLATERALS: Map<(&Addr, &str), Collateral> = Map::new("collaterals");
 pub const DEBTS: Map<(&Addr, &str), Debt> = Map::new("debts");
