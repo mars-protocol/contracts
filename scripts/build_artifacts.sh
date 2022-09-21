9b520a9ae1381efc41711c9924d79f1bb16922a1
Piotr Babel <piotr.babel@gmail.com>
2022-09-21 21:06:23 +0200

    test

--- scripts/build_artifacts.sh
+++ scripts/build_artifacts.sh
@@ -6,6 +6,7 @@ set -o pipefail
 cd ..
 
 
+
 docker run --rm -v "$(pwd)":/code \
     --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
     --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
