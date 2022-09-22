ddeb056b5cc2d10bc3fa6433ab6234c9751b84ba
Piotr Babel <piotr.babel@gmail.com>
2022-09-22 14:02:00 +0200

    MP-1089. Init multitest.

--- contracts/address-provider/tests/helpers.rs
+++ contracts/address-provider/tests/helpers.rs
@@ -1,7 +1,8 @@
 use cosmwasm_std::testing::{
     mock_dependencies_with_balance, mock_env, mock_info, MockApi, MockQuerier, MockStorage,
 };
-use cosmwasm_std::{from_binary, Deps, OwnedDeps};
+use cosmwasm_std::{from_binary, Addr, Deps, OwnedDeps};
+use cw_multi_test::BasicApp;
 use mars_address_provider::contract::{instantiate, query};
 
 use mars_outpost::address_provider::{InstantiateMsg, QueryMsg};
@@ -25,4 +26,8 @@ pub fn th_setup() -> OwnedDeps<MockStorage, MockApi, MockQuerier> {
 
 pub fn th_query<T: serde::de::DeserializeOwned>(deps: Deps, msg: QueryMsg) -> T {
     from_binary(&query(deps, mock_env(), msg).unwrap()).unwrap()
+}
+
+pub fn instantiate_address_provider(app: &mut BasicApp) -> Addr {
+    mars_testing::mock_multitest::instantiate_address_provider(app)
 }
