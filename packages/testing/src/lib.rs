dbdc182d34f95fc37b2d0dd53c90dc8636abf166
Piotr Babel <piotr.babel@gmail.com>
2022-10-27 23:33:34 +0200

    Fix to excluding multi-test deps from wasm builds.

--- packages/testing/src/lib.rs
+++ packages/testing/src/lib.rs
@@ -1,29 +1,19 @@
+#![cfg(not(target_arch = "wasm32"))]
+
 extern crate core;
 
 /// cosmwasm_std::testing overrides and custom test helpers
-#[cfg(not(target_arch = "wasm32"))]
 mod helpers;
-#[cfg(not(target_arch = "wasm32"))]
 mod incentives_querier;
-#[cfg(not(target_arch = "wasm32"))]
 mod mars_mock_querier;
-#[cfg(not(target_arch = "wasm32"))]
 mod mock_address_provider;
-#[cfg(not(target_arch = "wasm32"))]
 mod mocks;
-#[cfg(not(target_arch = "wasm32"))]
 mod oracle_querier;
-#[cfg(not(target_arch = "wasm32"))]
 mod osmosis_querier;
-#[cfg(not(target_arch = "wasm32"))]
 mod red_bank_querier;
 
-#[cfg(not(target_arch = "wasm32"))]
 pub use helpers::*;
-#[cfg(not(target_arch = "wasm32"))]
 pub use mars_mock_querier::MarsMockQuerier;
-#[cfg(not(target_arch = "wasm32"))]
 pub use mocks::*;
 
-#[cfg(not(target_arch = "wasm32"))]
 pub mod integration;
