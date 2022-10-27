279876bb98e4eece4412f2f3492c59a4ffcded81
brimigs <85972460+brimigs@users.noreply.github.com>
2022-10-27 11:51:11 -0400

    incentives user flow test

--- integration-tests/tests/test_incentives.rs
+++ integration-tests/tests/test_incentives.rs
@@ -1,8 +1,8 @@
-use cosmwasm_std::{Addr, BlockInfo, coin, Uint128, Uint64};
+use cosmwasm_std::{coin, Addr, BlockInfo, Uint128, Uint64};
 
 use mars_testing::integration::mock_env::MockEnvBuilder;
 
-use crate::helpers::{default_asset_params};
+use crate::helpers::default_asset_params;
 
 mod helpers;
 
@@ -19,34 +19,35 @@ fn test_rewards_claim() {
 
     // fund user wallet account with usdc
     let user = Addr::unchecked("user_a");
-    let funded_usdc = 10_000_000_000u128;
-    mock_env.fund_account(&user, &[coin(funded_usdc, "uusdc")]);
+    let funded_amt = 10_000_000_000u128;
+    mock_env.fund_account(&user, &[coin(funded_amt, "uusdc")]);
+    // mock_env.fund_account(&user, &[coin(funded_amt, "umars")]);
+
+    // fund incentives contract
+    mock_env.fund_account(&incentives.contract_addr, &[coin(funded_amt, "umars")]);
 
     // user deposits usdc
-    red_bank.deposit(&mut mock_env, &user, coin(funded_usdc, "uusdc")).unwrap();
+    red_bank.deposit(&mut mock_env, &user, coin(funded_amt, "uusdc")).unwrap();
     let balance = mock_env.query_balance(&user, "uusdc").unwrap();
     assert_eq!(balance.amount, Uint128::zero());
+    let mars_balance = mock_env.query_balance(&user, "umars").unwrap();
+    assert_eq!(mars_balance.amount, Uint128::zero());
     let user_collateral = red_bank.query_user_collateral(&mut mock_env, &user, "uusdc");
-    assert_eq!(user_collateral.amount.u128(), funded_usdc);
+    assert_eq!(user_collateral.amount.u128(), funded_amt);
 
     let rewards_balance = incentives.query_unclaimed_rewards(&mut mock_env, &user);
     assert_eq!(rewards_balance, Uint128::zero());
 
-    let info = mock_env.app.block_info();
-    mock_env.app.set_block(BlockInfo{
-        height: info.height + 100,
-        time: info.time.plus_seconds(86400), // 24 hours
-        chain_id: info.chain_id,
-    });
+    mock_env.increment_by_time(86400); // 24 hours
 
     let rewards_balance = incentives.query_unclaimed_rewards(&mut mock_env, &user);
-    assert_eq!(rewards_balance, Uint128::new(86400 * 10));
+    assert_eq!(rewards_balance, Uint128::new(864000));
 
     //claim rewards
     incentives.claim_rewards(&mut mock_env, &user).unwrap();
 
-    let balance = mock_env.query_balance(&user, "uusdc").unwrap();
-    assert_eq!(balance.amount, Uint128::new(86400 * 10));
+    let balance = mock_env.query_balance(&user, "umars").unwrap();
+    assert_eq!(balance.amount, Uint128::new(864000));
 
     let rewards_balance = incentives.query_unclaimed_rewards(&mut mock_env, &user);
     assert_eq!(rewards_balance, Uint128::zero());
