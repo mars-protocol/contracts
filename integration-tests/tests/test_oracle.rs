39ac573dea1eb7322a30f30cbbc1b1914139f44c
brimigs <85972460+brimigs@users.noreply.github.com>
2022-11-01 00:13:16 -0400

    set up SPOT & TWAP

--- integration-tests/tests/test_oracle.rs
+++ integration-tests/tests/test_oracle.rs
@@ -1,5 +1,5 @@
 use crate::helpers::default_asset_params;
-use cosmwasm_std::{coin, Addr, Decimal, Uint128};
+use cosmwasm_std::{Addr, Uint128};
 use mars_testing::integration::mock_env::MockEnvBuilder;
 
 mod helpers;
@@ -9,19 +9,22 @@ fn spot_test() {
     let owner = Addr::unchecked("owner");
     let mut mock_env = MockEnvBuilder::new(None, owner).build();
 
-    //set price source to SPOT
+    //setup oracle with SPOT price source
     let oracle = mock_env.oracle.clone();
     oracle.set_price_source_spot(&mut mock_env, "uatom", 1);
-    oracle.set_price_source_spot(&mut mock_env, "uosmo", 2);
+
     let red_bank = mock_env.red_bank.clone();
     red_bank.init_asset(&mut mock_env, "uatom", default_asset_params());
-    red_bank.init_asset(&mut mock_env, "uosmo", default_asset_params());
 
     let atom_price = oracle.query_asset_price(&mut mock_env, "uatom");
-    println!("{}", atom_price);
-    // assert_ne!(atom_price, 1);
+    assert_ne!(atom_price, Uint128::zero());
+}
 
-    let osmo_price = oracle.query_asset_price(&mut mock_env, "uosmo");
-    println!("{}", osmo_price);
-    // assert_ne!(osmo_price, 1);
+fn twap_test() {
+    let owner = Addr::unchecked("owner");
+    let mut mock_env = MockEnvBuilder::new(None, owner).build();
+
+    //setup oracle with TWAP price source
+    let oracle = mock_env.oracle.clone();
+    oracle.set_price_source_twap(&mut mock_env, "uatom", 1, 1800);
 }
