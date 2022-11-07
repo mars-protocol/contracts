5282ac6f16c9755e320c31363b12156186b8f9a3
brimigs <85972460+brimigs@users.noreply.github.com>
2022-11-07 12:25:56 -0500

    Update integration-tests/tests/test_incentives.rs
    Co-authored-by: piobab <piotr.babel@gmail.com>

--- integration-tests/tests/test_incentives.rs
+++ integration-tests/tests/test_incentives.rs
@@ -294,7 +294,7 @@ fn test_stopping_incentives() {
     let rewards_balance = incentives.query_unclaimed_rewards(&mut mock_env, &user);
     assert_eq!(rewards_balance, Uint128::new(432000));
 
-    //restart incentives
+    // restart incentives
     incentives.set_asset_incentive(&mut mock_env, "uusdc", 5);
 
     mock_env.increment_by_time(43200); // 12 hours
