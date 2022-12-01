bdc541cf8c9529eecd55bbccf1bc8736a9417c39
piobab <piotr.babel@gmail.com>
2022-12-01 19:21:19 +0100

    Use v2 queries. (#49)
    * Use v2 queries.
    
    * Bump osmosis deps.
    
    * Use v1 TWAP query.

--- packages/chains/osmosis/src/helpers.rs
+++ packages/chains/osmosis/src/helpers.rs
@@ -1,12 +1,13 @@
 use std::str::FromStr;
 
-use cosmwasm_std::{Decimal, Empty, QuerierWrapper, QueryRequest, StdResult};
+use cosmwasm_std::{
+    coin, Decimal, Empty, QuerierWrapper, QueryRequest, StdError, StdResult, Uint128,
+};
 
 use osmosis_std::shim::Timestamp;
 use osmosis_std::types::cosmos::base::v1beta1::Coin;
-use osmosis_std::types::osmosis::gamm::v1beta1::{
-    GammQuerier, PoolAsset, PoolParams, QueryPoolRequest,
-};
+use osmosis_std::types::osmosis::gamm::v1beta1::{PoolAsset, PoolParams, QueryPoolRequest};
+use osmosis_std::types::osmosis::gamm::v2::GammQuerier;
 use osmosis_std::types::osmosis::twap::v1beta1::TwapQuerier;
 
 use serde::{Deserialize, Serialize};
@@ -22,6 +23,21 @@ pub struct Pool {
     pub pool_assets: Vec<PoolAsset>,
     pub total_shares: Option<Coin>,
     pub total_weight: String,
+}
+
+impl Pool {
+    /// Unwraps Osmosis coin into Cosmwasm coin
+    pub fn unwrap_coin(osmosis_coin: &Option<Coin>) -> StdResult<cosmwasm_std::Coin> {
+        let osmosis_coin = match osmosis_coin {
+            None => return Err(StdError::generic_err("missing coin")), // just in case, it shouldn't happen
+            Some(osmosis_coin) => osmosis_coin,
+        };
+        let cosmwasm_coin = coin(
+            Uint128::from_str(&osmosis_coin.amount)?.u128(),
+            &osmosis_coin.denom,
+        );
+        Ok(cosmwasm_coin)
+    }
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
@@ -50,13 +66,10 @@ pub fn query_spot_price(
     base_denom: &str,
     quote_denom: &str,
 ) -> StdResult<Decimal> {
-    // NOTE: Currency pair consists of base and quote asset (base/quote). Spot query has it swapped.
-    // For example:
-    // if we want to check the price ATOM/OSMO then we pass base_asset = OSMO, quote_asset = ATOM
     let spot_price_res = GammQuerier::new(querier).spot_price(
         pool_id,
-        quote_denom.to_string(),
         base_denom.to_string(),
+        quote_denom.to_string(),
     )?;
     let price = Decimal::from_str(&spot_price_res.spot_price)?;
     Ok(price)
@@ -64,6 +77,7 @@ pub fn query_spot_price(
 
 /// Query the twap price of a coin, denominated in OSMO.
 /// `start_time` must be within 48 hours of current block time.
+#[allow(deprecated)] // FIXME: arithmetic_twap_to_now shouldn't be deprecated, make clippy happy for now
 pub fn query_twap_price(
     querier: &QuerierWrapper,
     pool_id: u64,
@@ -82,4 +96,45 @@ pub fn query_twap_price(
     )?;
     let price = Decimal::from_str(&arithmetic_twap_res.arithmetic_twap)?;
     Ok(price)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_unwrapping_coin() {
+        let pool = Pool {
+            id: "1111".to_string(),
+            address: "".to_string(),
+            pool_params: None,
+            future_pool_governor: "".to_string(),
+            pool_assets: vec![
+                PoolAsset {
+                    token: Some(Coin {
+                        denom: "denom_1".to_string(),
+                        amount: "123".to_string(),
+                    }),
+                    weight: "500".to_string(),
+                },
+                PoolAsset {
+                    token: Some(Coin {
+                        denom: "denom_2".to_string(),
+                        amount: "430".to_string(),
+                    }),
+                    weight: "500".to_string(),
+                },
+            ],
+            total_shares: None,
+            total_weight: "".to_string(),
+        };
+
+        let res_err = Pool::unwrap_coin(&pool.total_shares).unwrap_err();
+        assert_eq!(res_err, StdError::generic_err("missing coin"));
+
+        let res = Pool::unwrap_coin(&pool.pool_assets[0].token).unwrap();
+        assert_eq!(res, coin(123, "denom_1"));
+        let res = Pool::unwrap_coin(&pool.pool_assets[1].token).unwrap();
+        assert_eq!(res, coin(430, "denom_2"));
+    }
 }
