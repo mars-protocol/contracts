62ec93be8554067de7e870651594f2ac98c1b6a0
Piotr Babel <piotr.babel@gmail.com>
2023-01-03 17:46:01 +0100

    Simplify tests for ownership.

--- contracts/incentives/tests/test_update_owner.rs
+++ contracts/incentives/tests/test_update_owner.rs
@@ -1,6 +1,6 @@
 use crate::helpers::{th_query, th_setup};
 use cosmwasm_std::testing::{mock_env, mock_info};
-use cw_controllers_admin_fork::AdminError::{NotAdmin, NotProposedAdmin};
+use cw_controllers_admin_fork::AdminError::NotAdmin;
 use cw_controllers_admin_fork::AdminUpdate;
 use mars_incentives::contract::execute;
 use mars_incentives::ContractError;
@@ -18,14 +18,14 @@ fn test_initialized_state() {
 }
 
 #[test]
-fn test_propose_new_owner() {
+fn test_update_owner() {
     let mut deps = th_setup();
 
     let original_config: ConfigResponse = th_query(deps.as_ref(), QueryMsg::Config {});
 
-    let new_admin = "new_admin";
+    let new_owner = "new_admin";
 
-    // only admin can propose new admins
+    // only owner can propose new owners
     let bad_guy = "bad_guy";
     let err = execute(
         deps.as_mut(),
@@ -38,108 +38,31 @@ fn test_propose_new_owner() {
     .unwrap_err();
     assert_eq!(err, ContractError::AdminError(NotAdmin {}));
 
+    // propose new owner
     execute(
         deps.as_mut(),
         mock_env(),
         mock_info(&original_config.owner.clone().unwrap(), &[]),
         ExecuteMsg::UpdateOwner(AdminUpdate::ProposeNewAdmin {
-            proposed: new_admin.to_string(),
+            proposed: new_owner.to_string(),
         }),
     )
     .unwrap();
-
     let new_config: ConfigResponse = th_query(deps.as_ref(), QueryMsg::Config {});
-
-    assert_eq!(new_config.owner, original_config.owner);
+    assert_eq!(new_config.owner.unwrap(), original_config.owner.clone().unwrap());
     assert_ne!(new_config.proposed_new_owner, original_config.proposed_new_owner);
-    assert_eq!(new_config.proposed_new_owner, Some(new_admin.to_string()));
-}
+    assert_eq!(new_config.proposed_new_owner.unwrap(), new_owner.to_string());
 
-#[test]
-fn test_clear_proposed_owner() {
-    let mut deps = th_setup();
-
-    let original_config: ConfigResponse = th_query(deps.as_ref(), QueryMsg::Config {});
-
-    let new_admin = "new_admin";
-
+    // accept ownership
     execute(
         deps.as_mut(),
         mock_env(),
-        mock_info(&original_config.owner.clone().unwrap(), &[]),
-        ExecuteMsg::UpdateOwner(AdminUpdate::ProposeNewAdmin {
-            proposed: new_admin.to_string(),
-        }),
-    )
-    .unwrap();
-
-    let interim_config: ConfigResponse = th_query(deps.as_ref(), QueryMsg::Config {});
-    assert_eq!(interim_config.proposed_new_owner, Some(new_admin.to_string()));
-
-    // only admin can clear
-    let bad_guy = "bad_guy";
-    let err = execute(
-        deps.as_mut(),
-        mock_env(),
-        mock_info(bad_guy, &[]),
-        ExecuteMsg::UpdateOwner(AdminUpdate::ClearProposed),
-    )
-    .unwrap_err();
-    assert_eq!(err, ContractError::AdminError(NotAdmin {}));
-
-    execute(
-        deps.as_mut(),
-        mock_env(),
-        mock_info(&original_config.owner.clone().unwrap(), &[]),
-        ExecuteMsg::UpdateOwner(AdminUpdate::ClearProposed),
-    )
-    .unwrap();
-
-    let latest_config: ConfigResponse = th_query(deps.as_ref(), QueryMsg::Config {});
-
-    assert_eq!(latest_config.owner, original_config.owner);
-    assert_ne!(latest_config.proposed_new_owner, interim_config.proposed_new_owner);
-    assert_eq!(latest_config.proposed_new_owner, None);
-}
-
-#[test]
-fn test_accept_owner_role() {
-    let mut deps = th_setup();
-
-    let original_config: ConfigResponse = th_query(deps.as_ref(), QueryMsg::Config {});
-
-    let new_admin = "new_admin";
-
-    execute(
-        deps.as_mut(),
-        mock_env(),
-        mock_info(&original_config.owner.clone().unwrap(), &[]),
-        ExecuteMsg::UpdateOwner(AdminUpdate::ProposeNewAdmin {
-            proposed: new_admin.to_string(),
-        }),
-    )
-    .unwrap();
-
-    // Only proposed admin can accept
-    let err = execute(
-        deps.as_mut(),
-        mock_env(),
-        mock_info(&original_config.owner.unwrap(), &[]),
-        ExecuteMsg::UpdateOwner(AdminUpdate::AcceptProposed),
-    )
-    .unwrap_err();
-    assert_eq!(err, ContractError::AdminError(NotProposedAdmin {}));
-
-    execute(
-        deps.as_mut(),
-        mock_env(),
-        mock_info(new_admin, &[]),
+        mock_info(new_owner, &[]),
         ExecuteMsg::UpdateOwner(AdminUpdate::AcceptProposed),
     )
     .unwrap();
-
     let new_config: ConfigResponse = th_query(deps.as_ref(), QueryMsg::Config {});
-
-    assert_eq!(new_config.owner.unwrap(), new_admin.to_string());
+    assert_eq!(new_config.owner.clone().unwrap(), new_owner.to_string());
+    assert_ne!(new_config.owner, original_config.owner);
     assert_eq!(new_config.proposed_new_owner, None);
 }
