fb74fa7e15f807ee5a264b187c70ebbce8238cac
Piotr Babel <piotr.babel@gmail.com>
2023-01-28 14:02:09 +0100

    Be consisten with pool_id representation as u64.

--- contracts/rewards-collector/osmosis/tests/test_routes.rs
+++ contracts/rewards-collector/osmosis/tests/test_routes.rs
@@ -2,10 +2,11 @@ use cosmwasm_std::testing::mock_env;
 use mars_owner::OwnerError::NotOwner;
 use mars_red_bank_types::rewards_collector::{QueryMsg, RouteResponse};
 use mars_rewards_collector_base::{ContractError, Route};
-use mars_rewards_collector_osmosis::{contract::entry::execute, msg::ExecuteMsg, OsmosisRoute};
+use mars_rewards_collector_osmosis::{
+    contract::entry::execute, msg::ExecuteMsg, route::SwapAmountInRoute, OsmosisRoute,
+};
 use mars_testing::mock_info;
 use mars_utils::error::ValidationError;
-use osmosis_std::types::osmosis::gamm::v1beta1::SwapAmountInRoute;
 
 use crate::helpers::mock_routes;
 
