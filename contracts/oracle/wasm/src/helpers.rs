8e54158b7990b85d48f922b4db264f6124a57e61
Sturdy <91910406+apollo-sturdy@users.noreply.github.com>
2023-04-11 19:19:30 +0200

    fix: fix bug in assert_astroport_pair_contains_denoms

--- contracts/oracle/wasm/src/helpers.rs
+++ contracts/oracle/wasm/src/helpers.rs
@@ -32,20 +32,61 @@ pub fn astro_native_asset(denom: impl Into<String>, amount: impl Into<Uint128>) 
     }
 }
 
+/// Asserts that the pair contains exactly the specified denoms.
 pub fn assert_astroport_pair_contains_denoms(
     pair_info: &PairInfo,
     denoms: &[&str],
 ) -> ContractResult<()> {
-    for denom in denoms {
-        if !pair_info.asset_infos.contains(&astro_native_info(denom)) {
-            return Err(ContractError::InvalidPriceSource {
-                reason: format!(
-                    "pair {} does not contain the denom {}",
-                    pair_info.contract_addr, denom
-                ),
-            });
-        }
-    }
+    // sort denoms to compare them
+    let mut pair_denoms: Vec<String> =
+        pair_info.asset_infos.iter().map(|a| a.to_string()).collect();
+    let mut denoms = denoms.to_vec();
+    denoms.sort();
+    pair_denoms.sort();
 
+    if denoms != pair_denoms {
+        return Err(ContractError::InvalidPriceSource {
+            reason: format!(
+                "pair {} does not contain the denoms {:?}",
+                pair_info.contract_addr, denoms
+            ),
+        });
+    }
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use cosmwasm_std::Addr;
+
+    use super::*;
+
+    #[test]
+    fn test_assert_astroport_pair_contains_denoms() {
+        let pair_info = PairInfo {
+            contract_addr: Addr::unchecked("pair_contract"),
+            asset_infos: vec![
+                astro_native_info("uusd"),
+                astro_native_info("uatom"),
+                astro_native_info("uluna"),
+            ],
+            liquidity_token: Addr::unchecked("liquidity_token"),
+            pair_type: astroport::factory::PairType::Xyk {},
+        };
+
+        assert_astroport_pair_contains_denoms(&pair_info, &["uusd", "uatom", "uluna"]).unwrap();
+        assert_astroport_pair_contains_denoms(&pair_info, &["uusd", "uluna", "uatom"]).unwrap();
+        assert_astroport_pair_contains_denoms(&pair_info, &["uatom", "uusd", "uluna"]).unwrap();
+        assert_astroport_pair_contains_denoms(&pair_info, &["uusd"]).unwrap_err();
+        assert_astroport_pair_contains_denoms(&pair_info, &["uatom"]).unwrap_err();
+        assert_astroport_pair_contains_denoms(&pair_info, &["uluna"]).unwrap_err();
+
+        assert_astroport_pair_contains_denoms(&pair_info, &["uusd", "uatom", "uluna", "ukrw"])
+            .unwrap_err();
+        assert_astroport_pair_contains_denoms(&pair_info, &["uusd", "uatom", "ukrw"]).unwrap_err();
+        assert_astroport_pair_contains_denoms(&pair_info, &["uusd", "ukrw"]).unwrap_err();
+        assert_astroport_pair_contains_denoms(&pair_info, &["uatom", "ukrw"]).unwrap_err();
+        assert_astroport_pair_contains_denoms(&pair_info, &["uluna", "ukrw"]).unwrap_err();
+        assert_astroport_pair_contains_denoms(&pair_info, &["ukrw"]).unwrap_err();
+    }
 }
