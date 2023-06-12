905b75f7ff71f055af1e174bf695c032f40a0306
Pacman <pacman@apollo.farm>
2023-06-12 17:11:33 +0200

    test: add UT:s for Swapper adapter

--- packages/types/src/swapper/adapter.rs
+++ packages/types/src/swapper/adapter.rs
@@ -50,3 +50,61 @@ impl Swapper {
         }))
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use cosmwasm_std::testing::MockApi;
+
+    use super::*;
+
+    #[test]
+    fn test_swapper_unchecked_from_swapper() {
+        let swapper = Swapper::new(Addr::unchecked("swapper"));
+        let swapper_unchecked = SwapperUnchecked::from(swapper.clone());
+        assert_eq!(swapper_unchecked.address(), "swapper");
+        assert_eq!(swapper_unchecked.check(&MockApi::default()).unwrap(), swapper);
+    }
+
+    #[test]
+    fn test_swapper_unchecked_check() {
+        let swapper = SwapperUnchecked::new("swapper".to_string());
+        assert_eq!(
+            swapper.check(&MockApi::default()).unwrap(),
+            Swapper::new(Addr::unchecked("swapper".to_string()))
+        );
+    }
+
+    #[test]
+    fn test_new_and_address() {
+        // Swapper
+        let swapper = Swapper::new(Addr::unchecked("swapper"));
+        assert_eq!(swapper.address(), &Addr::unchecked("swapper"));
+
+        // SwapperUnchecked
+        let swapper_unchecked = SwapperUnchecked::new("swapper".to_string());
+        assert_eq!(swapper_unchecked.address(), "swapper");
+    }
+
+    #[test]
+    fn test_swapper_swap_exact_in_msg() {
+        let swapper = Swapper::new(Addr::unchecked("swapper"));
+        let coin_in = Coin::new(100, "in");
+        let denom_out = "out";
+        let slippage = Decimal::percent(1);
+
+        let msg = swapper.swap_exact_in_msg(&coin_in, denom_out, slippage).unwrap();
+        assert_eq!(
+            msg,
+            CosmosMsg::Wasm(WasmMsg::Execute {
+                contract_addr: "swapper".to_string(),
+                msg: to_binary(&ExecuteMsg::<Empty>::SwapExactIn {
+                    coin_in: coin_in.clone(),
+                    denom_out: denom_out.to_string(),
+                    slippage,
+                })
+                .unwrap(),
+                funds: vec![coin_in.clone()],
+            })
+        );
+    }
+}
