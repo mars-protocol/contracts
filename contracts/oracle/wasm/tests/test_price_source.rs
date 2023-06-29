7296f86dee6e66da5805a685e8ac075e29b79a25
Sturdy <91910406+apollo-sturdy@users.noreply.github.com>
2023-06-29 16:01:34 +0200

    style: clippy

--- contracts/oracle/wasm/tests/test_price_source.rs
+++ contracts/oracle/wasm/tests/test_price_source.rs
@@ -239,7 +239,7 @@ fn test_query_astroport_twap_price_with_only_one_snapshot() {
     );
 
     let price_source = WasmPriceSourceUnchecked::AstroportTwap {
-        pair_address: pair_address.clone(),
+        pair_address,
         route_assets: vec![],
         tolerance: 3,
         window_size: 2,
