4efa1b8c4184f0cae60eb02673c40927a164cb37
Sturdy <91910406+apollo-sturdy@users.noreply.github.com>
2023-07-06 15:09:15 +0200

    fix: Only increment whitelist counter if adding a new denom (#228)
    * fix: Only increment whitelist counter if adding a new
    denom
    
    * feat: error when trying to remove non-existing denom
    
    * test: More whitelist tests
    
    * doc: correct comment in test_whitelist.rs
    
    ---------
    
    Co-authored-by: pacmanifold <105084485+pacmanifold@users.noreply.github.com>

--- contracts/incentives/src/contract.rs
+++ contracts/incentives/src/contract.rs
@@ -162,6 +162,13 @@ pub fn execute_update_whitelist(
     let mut whitelist_count = prev_whitelist_count;
 
     for denom in remove_denoms.iter() {
+        // If denom is not on the whitelist, we can't remove it
+        if !WHITELIST.has(deps.storage, denom) {
+            return Err(ContractError::NotWhitelisted {
+                denom: denom.clone(),
+            });
+        }
+
         whitelist_count -= 1;
 
         // Before removing from whitelist we must handle ongoing incentives,
@@ -206,12 +213,16 @@ pub fn execute_update_whitelist(
     }
 
     for (denom, min_emission) in add_denoms.iter() {
-        // Check that we are not exceeding the max whitelist limit
-        whitelist_count += 1;
-        if whitelist_count > config.max_whitelisted_denoms {
-            return Err(ContractError::MaxWhitelistLimitReached {
-                max_whitelist_limit: config.max_whitelisted_denoms,
-            });
+        // If the denom is not already whitelisted, increase the counter and check that we are not
+        // exceeding the max whitelist limit. If the denom is already whitelisted, we don't need
+        // to change the counter and instead just update the min_emission.
+        if !WHITELIST.has(deps.storage, denom) {
+            whitelist_count += 1;
+            if whitelist_count > config.max_whitelisted_denoms {
+                return Err(ContractError::MaxWhitelistLimitReached {
+                    max_whitelist_limit: config.max_whitelisted_denoms,
+                });
+            }
         }
 
         validate_native_denom(denom)?;
