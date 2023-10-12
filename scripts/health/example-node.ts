dd74ef80aa6f78e2268b0cb2bcbbcabb56e10e04
piobab <piobab0x@proton.me>
2023-10-12 12:28:53 +0200

    Clean addresses.

--- scripts/health/example-node.ts
+++ scripts/health/example-node.ts
@@ -1,16 +1,15 @@
 import { DataFetcher } from './DataFetcher'
 import { compute_health_js, max_withdraw_estimate_js, max_borrow_estimate_js } from './pkg-node'
-import { osmosisTestnetConfig } from '../deploy/osmosis/testnet-config'
-import OsmosisAddresses from '../deploy/addresses/osmo-test-5-testnet-deployer-owner.json'
+import OsmosisAddresses from '../deploy/addresses/devnet-deployer-owner.json'
 ;(async () => {
   const dataFetcher = new DataFetcher(
     compute_health_js,
     max_withdraw_estimate_js,
     max_borrow_estimate_js,
     OsmosisAddresses.creditManager,
-    OsmosisAddresses.oracle.addr,
-    osmosisTestnetConfig.params.addr,
-    osmosisTestnetConfig.chain.rpcEndpoint,
+    OsmosisAddresses.oracle,
+    OsmosisAddresses.params,
+    'https://rpc.devnet.osmosis.zone',
   )
   const health = await dataFetcher.computeHealth('2')
   console.log(health)
