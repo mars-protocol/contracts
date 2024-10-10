ac7b273fbb85c8f0498e71ba7ace267e330544f6
Mark Watney <josiah@delphilabs.io>
2024-10-10 10:04:34 +0200

    remove migration error

--- contracts/incentives/src/error.rs
+++ contracts/incentives/src/error.rs
@@ -70,9 +70,6 @@ pub enum ContractError {
         denom: String,
     },
 
-    #[error("Invalid migration call")]
-    InvalidMigrationCall {},
-
     #[error("{0}")]
     Version(#[from] cw2::VersionError),
 
