9f1a40e0ae65ab5f85f8fd1df6911548a4de619d
piobab <piobab0x@proton.me>
2024-12-17 22:04:49 +0100

    MP-2562. Add missing connection between RC addr and acc id.

--- contracts/account-nft/src/migrations/v2_1_0.rs
+++ contracts/account-nft/src/migrations/v2_1_0.rs
@@ -1,16 +1,45 @@
-use cosmwasm_std::{DepsMut, Response};
+use cosmwasm_std::{Addr, DepsMut, Empty, MessageInfo, Response};
 use cw2::{assert_contract_version, set_contract_version};
 
 use crate::{
-    contract::{CONTRACT_NAME, CONTRACT_VERSION},
+    contract::{Parent, CONTRACT_NAME, CONTRACT_VERSION},
     error::ContractError,
 };
 
 const FROM_VERSION: &str = "2.0.0";
 
-pub fn migrate(deps: DepsMut) -> Result<Response, ContractError> {
-    // make sure we're migrating the correct contract and from the correct version
+const CREDIT_MANAGER_CONTRACT_ADDR: &str =
+    "osmo1f2m24wktq0sw3c0lexlg7fv4kngwyttvzws3a3r3al9ld2s2pvds87jqvf";
+
+/// Check Credit-Manager `config` query response:
+/// {
+///     ...
+///       "rewards_collector": {
+///         "address": "osmo1urvqe5mw00ws25yqdd4c4hlh8kdyf567mpcml7cdve9w08z0ydcqvsrgdy",
+///         "account_id": "2321"
+///       }
+///     }
+/// }
+const REWARDS_COLLECTOR_CONTRACT_ADDR: &str =
+    "osmo1urvqe5mw00ws25yqdd4c4hlh8kdyf567mpcml7cdve9w08z0ydcqvsrgdy";
+const REWARDS_COLLECTOR_ACC_ID: &str = "2321";
+
+pub fn migrate(mut deps: DepsMut) -> Result<Response, ContractError> {
+    // Make sure we're migrating the correct contract and from the correct version
     assert_contract_version(deps.storage, &format!("crates.io:{CONTRACT_NAME}"), FROM_VERSION)?;
+
+    // Create missing connection between Reward-Collector contract addr and acc id
+    Parent::default().mint(
+        deps.branch(),
+        MessageInfo {
+            sender: Addr::unchecked(CREDIT_MANAGER_CONTRACT_ADDR.to_string()),
+            funds: vec![],
+        },
+        REWARDS_COLLECTOR_ACC_ID.to_string(),
+        REWARDS_COLLECTOR_CONTRACT_ADDR.to_string(),
+        None,
+        Empty {},
+    )?;
 
     set_contract_version(deps.storage, format!("crates.io:{CONTRACT_NAME}"), CONTRACT_VERSION)?;
 
--- contracts/account-nft/tests/tests/test_migration_v2.rs
+++ contracts/account-nft/tests/tests/test_migration_v2.rs
@@ -1,6 +1,15 @@
-use cosmwasm_std::{attr, testing::mock_env, Empty, Event};
+use cosmwasm_std::{
+    attr, from_json,
+    testing::{mock_env, mock_info},
+    Empty, Event,
+};
 use cw2::{ContractVersion, VersionError};
-use mars_account_nft::{contract::migrate, error::ContractError};
+use cw721_base::InstantiateMsg;
+use mars_account_nft::{
+    contract::{execute, migrate, query, Parent},
+    error::ContractError,
+    state::NEXT_ID,
+};
 use mars_testing::mock_dependencies;
 
 #[test]
@@ -42,7 +51,85 @@ fn successful_migration() {
     cw2::set_contract_version(deps.as_mut().storage, "crates.io:mars-account-nft", "2.0.0")
         .unwrap();
 
+    let env = mock_env();
+    // Credit-Manager contract address
+    let owner = "osmo1f2m24wktq0sw3c0lexlg7fv4kngwyttvzws3a3r3al9ld2s2pvds87jqvf";
+    let owner_info = mock_info(owner, &[]);
+
+    // Init counter with 1
+    NEXT_ID.save(deps.as_mut().storage, &1).unwrap();
+
+    // Instantiate the contract
+    Parent::default()
+        .instantiate(
+            deps.as_mut(),
+            env.clone(),
+            owner_info.clone(),
+            InstantiateMsg {
+                name: "mock_nft".to_string(),
+                symbol: "MOCK".to_string(),
+                minter: owner.to_string(),
+            },
+        )
+        .unwrap();
+
+    // Mint a random token
+    execute(
+        deps.as_mut(),
+        env.clone(),
+        owner_info.clone(),
+        mars_types::account_nft::ExecuteMsg::Mint {
+            user: "user_1".to_string(),
+        },
+    )
+    .unwrap();
+
+    // Move counter to 3000
+    NEXT_ID.save(deps.as_mut().storage, &3000).unwrap();
+
+    // Mint a random token
+    execute(
+        deps.as_mut(),
+        env.clone(),
+        owner_info.clone(),
+        mars_types::account_nft::ExecuteMsg::Mint {
+            user: "user_2".to_string(),
+        },
+    )
+    .unwrap();
+
+    // Check if counter is moved to 3001
+    let next_id = NEXT_ID.load(deps.as_ref().storage).unwrap();
+    assert_eq!(next_id, 3001);
+
+    // Query should fail because token_id 2321 is not minted yet
+    query(
+        deps.as_ref(),
+        env.clone(),
+        mars_types::account_nft::QueryMsg::OwnerOf {
+            token_id: "2321".to_string(),
+            include_expired: None,
+        },
+    )
+    .unwrap_err();
+
     let res = migrate(deps.as_mut(), mock_env(), Empty {}).unwrap();
+
+    // Query should now return the owner of token_id 2321, which is Rewards-Collector contract address
+    let owner_of_res_binary = query(
+        deps.as_ref(),
+        env.clone(),
+        mars_types::account_nft::QueryMsg::OwnerOf {
+            token_id: "2321".to_string(),
+            include_expired: None,
+        },
+    )
+    .unwrap();
+    let owner_of_res: cw721::OwnerOfResponse = from_json(owner_of_res_binary).unwrap();
+    assert_eq!(
+        owner_of_res.owner,
+        "osmo1urvqe5mw00ws25yqdd4c4hlh8kdyf567mpcml7cdve9w08z0ydcqvsrgdy".to_string()
+    );
 
     assert_eq!(res.messages, vec![]);
     assert_eq!(res.events, vec![] as Vec<Event>);
